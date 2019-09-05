7cbbc4b295803e7a7a0f7f43c61c5d4e12f4dd9c
Istvan Molnar <istvan0988@gmail.com>
2019-09-05 12:03:00 +0200

    feat(details component initialized)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { MainComponent } from './components/main/main.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
