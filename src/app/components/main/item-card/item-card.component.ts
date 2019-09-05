4938c21134f791914347623e7414bef2b7c791b9
Andor Auber <andor.auber@gmail.com>
2019-09-05 11:25:25 +0200

    chore(item-card): initialize ItemCard Component

---
+++ src/app/components/main/item-card/item-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-card',
+  templateUrl: './item-card.component.html',
+  styleUrls: ['./item-card.component.sass']
+})
+export class ItemCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
