c59ce4ab9c8f49e394017581f7f5fa8e934e5b3c
Alpar <alpar1899@gmail.com>
2019-09-05 16:20:14 +0200

    feat: filter service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,26 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { MainComponent } from './components/main/main.component';
 import { SidebarComponent } from './components/main/sidebar/sidebar.component';
 import { FiltersComponent } from './components/main/filters/filters.component';
-import { SliderComponent } from './components/main/filters/slider/slider.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzTreeModule } from 'ng-zorro-antd/tree';
 
+import {MatSliderModule} from '@angular/material/slider';
+
 import { IconDefinition } from '@ant-design/icons-angular';
 import { NzIconModule, NZ_ICON_DEFAULT_TWOTONE_COLOR, NZ_ICONS } from 'ng-zorro-antd/icon';
+import { NzSliderModule } from 'ng-zorro-antd/slider';
 
 // Import what you need. RECOMMENDED. ✔️
 import { AccountBookFill, AlertFill, AlertOutline } from '@ant-design/icons-angular/icons';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 const icons: IconDefinition[] = [ AccountBookFill, AlertOutline, AlertFill ];
 
@@ -31,7 +37,6 @@ const icons: IconDefinition[] = [ AccountBookFill, AlertOutline, AlertFill ];
     AppComponent,
     MainComponent,
     FiltersComponent,
-    SliderComponent,
     SidebarComponent
   ],
   imports: [
@@ -45,10 +50,12 @@ const icons: IconDefinition[] = [ AccountBookFill, AlertOutline, AlertFill ];
     NgZorroAntdModule,
     NzButtonModule,
     NzIconModule,
+    NzSliderModule,
+    MatSliderModule
   ],
   providers: [
     { provide: NZ_ICON_DEFAULT_TWOTONE_COLOR, useValue: '#00ff00' }, // If not provided, Ant Design's official blue would be used
-    { provide: NZ_ICONS, useValue: icons }
+    { provide: NZ_ICONS, useValue: icons }, { provide: NZ_I18N, useValue: en_US }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/components/main/filters/filters.component.ts
+++ src/app/components/main/filters/filters.component.ts
@@ -1,42 +1,32 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { FilterService } from 'src/app/services/filter.service';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.sass']
 })
-export class FiltersComponent implements OnInit, OnChanges {
+export class FiltersComponent implements OnInit{
   filterForm: FormGroup;
-  priceValue: number;
-  isSelected = false;
-  constructor() { }
+
+  constructor(private filterService: FilterService) { }
 
   ngOnInit() {
     this.filterForm = new FormGroup({
-      priceRange: new FormControl('null'),
       colorDisplay: new FormControl(null),
       camera: new FormControl(null),
       wap: new FormControl(null)
-      // email: new FormControl(null),
-      // status: new FormControl('Stable')
     });
-    this.priceValue=this.filterForm.value.priceRange
-    console.log(this.filterForm);
   }
 
-  ngOnChanges(){
-    console.log(this.filterForm);
-
+  onSubmit() {
+    const data = {
+      colorDisplay: this.filterForm.value.colorDisplay,
+      camera: this.filterForm.value.camera,
+      wap: this.filterForm.value.wap
+    };
+    this.filterService.updateFilterValue(data);
   }
-
-  onSelect(){
-    this.isSelected = !this.isSelected;
-  }
-
-  onSubmit(){
-    console.log(this.filterForm);
-  }
-
-
 }
