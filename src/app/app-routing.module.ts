2b1b3e87e36a244307a306409ea68ca46e5e0f12
Andor Auber <andor.auber@gmail.com>
2019-09-05 11:49:25 +0200

    feat(): add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './components/main/main.component';
+
+const routes: Routes = [{ path: '', component: MainComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
