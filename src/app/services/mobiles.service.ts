a43ff89852c2596e9f3cf1f512136e93660070df
tothtomi <tothtomi7@gmail.com>
2019-09-05 19:58:35 +0200

    filter list-view with sidebar

--- src/app/services/mobiles.service.ts
+++ src/app/services/mobiles.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment'
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MobilesService {
 
+  private selectedPhonesSubject = new Subject<any>();
+  selectedPhones = this.selectedPhonesSubject.asObservable();
+
   constructor(private http: HttpClient) { }
 
+  getMobilesforSideBar() {
+    return this.http.get(`${environment.serverURL}/shop/sidebar`, {
+      headers: { 'content-type': 'application/json' },
+    });
+  }
   getMobiles(key?: string) {
     return this.http.get(`${environment.serverURL}/shop/${key}`, {
       headers: { 'content-type': 'application/json' },
-    });
+    }).subscribe(response => {
+      this.selectedPhonesSubject.next(response)
+    })
   }
 }
