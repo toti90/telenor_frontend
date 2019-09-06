3806bd4df0e0339b237f349a241fd7dbfd18e67f
BalazsMikola <mbalazs86@gmail.com>
2019-09-06 08:17:32 +0200

    feat: make shopping price dinamic

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -9,23 +9,29 @@ import { MobilesService } from 'src/app/services/mobiles.service';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  // private keys:string = '';
-  // private arrayOfKeys:string[] = [];
+  private totalPrice:number = 0;
   private mobiles: any[];
 
   constructor(private router: Router, private mobilesService: MobilesService) { }
 
   ngOnInit() {
     if(localStorage.getItem('cart')){
-      // this.keys = localStorage.getItem('cart');
-      // this.arrayOfKeys = localStorage.getItem('cart').split(',');
       this.mobilesService.phonesInCart.subscribe(response => {
         this.mobiles = response;
-        console.log(this.mobiles);
-        
+        this.totalPrice = 0;
+        this.mobiles.forEach(mobile => this.totalPrice += parseInt(mobile.price));
       });
       this.mobilesService.getMobilesForCart(localStorage.getItem('cart'));
     }
+  }
+
+  removeItem(key){
+    let keyArray = localStorage.getItem('cart').split(',');
+    let index:number = keyArray.indexOf(key);
+    keyArray.splice(index,1);
+    localStorage.setItem('cart',keyArray.join(','));
+    this.mobilesService.getMobilesForCart(localStorage.getItem('cart'));
+    
   }
 
   onRedirect(route: string) {
